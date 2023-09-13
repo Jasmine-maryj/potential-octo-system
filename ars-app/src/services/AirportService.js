5ac4e3fe23c64550e919ef357ca4ce876e7c77c4
Jasmine Mary <jasminemary630@gmail.com>
2023-09-13 21:06:46 +0530

    Change api routes

--- ars-app/src/services/AirportService.js
+++ ars-app/src/services/AirportService.js
@@ -7,7 +7,7 @@ const ARS_BASE_REST_API_URL = "http://localhost:8080/api/v1";
 class AirportService{
 
     getAllAirports(){
-        return axios.get(ARS_BASE_REST_API_URL+"/aiports/all");
+        return axios.get(ARS_BASE_REST_API_URL+"/airports/all");
     }
 
     createAiport(airport){
